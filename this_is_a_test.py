cc9d432485e78c74f37dad9a2385693cde263df2
mgar216 <28987804+mgar216@users.noreply.github.com>
2023-05-25 20:58:42 -0600

    Add files via upload

---
+++ this_is_a_test.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+testing = {
+    'site_one': 'archgroup.com',
+    'sitesite_two': 'archre.com',
+    'api_key': 'mlHUkeu67ZfukAhbB2pWSTjC3WMdF2LzTS7a9gVl0DwT3K0kNe4hWuME5bgN84j3CU1a2ejNJBCEkFVesABbkwzpwO2U6skgU1ubmvmYTOP3XoM8BMJU1BpfMzSWdejZ',
+    'secret_key': 'qsQjb2J3Yeq9LR7uludgehAgQ9AdP6ezmhDRQFDJEFtlTCPMHii5cC1zLLDDFE2kJRqhBxNcdkI4wplRAtLFpb5nr3X133UAC182WjNVdUVEz22PMSIgYYeToXcU1XBb',
+    'password': 'Tst#Pas$w0rd274D845##44$'
+}
